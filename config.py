02e21b70349190a08a8c16e1551b51b8338e2f86
seven <199032163@qq.com>
2015-02-27 15:46:53 +0800

    UPDATE
    DATABASE URI

--- config.py
+++ config.py
@@ -1,13 +1,21 @@
 # -*- coding: utf-8 -*-
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
+if os.environ.get('DATABASE_URL') is None:
+    SQLALCHEMY_DATABASE_URI = ('sqlite:///' + os.path.join(basedir, 'app.db') +
+                               '?check_same_thread=False')
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_RECORD_QUERIES = True
 WHOOSH_BASE = os.path.join(basedir, 'search.db')
 
 # Whoosh does not work on Heroku
 WHOOSH_ENABLED = os.environ.get('HEROKU') is None
+
+# slow database query threshold (in seconds)
+DATABASE_QUERY_TIMEOUT = 0.5
 
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
